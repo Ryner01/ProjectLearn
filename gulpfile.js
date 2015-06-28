b13709581df5ff03021f60644f4f59c650546b80
Predrag <prediik@gmail.com>
2015-06-28 05:22:51 +0200

    basic HTML and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var uglify = require('gulp-uglify');
+livereload = require('gulp-livereload');
 
 gulp.task('less', function () {
   return gulp.src('./*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload());
 });
+gulp.task('js',function(){
+	return gulp.src('./js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist'))
+		.pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('./*.less', ['less']);
+});	
